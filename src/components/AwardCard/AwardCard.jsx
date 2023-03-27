a3d148af4f3162f8255751a1b783f4fe2daff64c
sherstnew <sherstnev.denis.v@gmail.com>
2023-03-27 22:58:52 +0300

    color fix

--- src/components/AwardCard/AwardCard.jsx
+++ src/components/AwardCard/AwardCard.jsx
@@ -3,7 +3,7 @@ import awardImg from '../../static/images/award.png';
 
 export const AwardCard = ({award}) => {
   return (
-    <div className={styles.award} style={{backgroundImage: `linear-gradient(180deg, rgba(255, 255, 255, 0.2), rgba(0, 0, 0, 1) 100%), url(${award.img ? award.img : awardImg})`}}>
+    <div className={styles.award} style={{backgroundImage: `linear-gradient(180deg, rgba(0, 0, 0, 0), rgba(0, 0, 0, 1) 100%), url(${award.img ? award.img : awardImg})`}}>
       <div className={styles.award__wrapper}>
         <div className={styles.award__title}>{award.name}</div>
         <div className={styles.award__about}>{award.about}</div>
