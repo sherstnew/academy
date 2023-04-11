6153cfe1dfa0beba276c40b7ff945fd56d0be2e6
sherstnew <sherstnev.denis.v@gmail.com>
2023-04-11 17:21:01 +0300

    config

--- src/academy.config.js
+++ src/academy.config.js
@@ -1,5 +1,5 @@
 export const ACADEMYCONFIG = {
-  HOST: process.env.NODE_ENV === 'development' ? 'http://localhost:5000' : 'https://dull-ruby-fawn-cuff.cyclic.app',
+  HOST: process.env.NODE_ENV === 'development' ? 'http://localhost:5000' : ' https://academy-server.cyclic.app',
   TG: 'https://t.me/academypfv',
   INST: 'https://www.instagram.com/academy_pfv',
   VK: 'https://vk.com/academypfv',
