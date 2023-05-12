3321641a3a58452ab0dc1f5a5c5863f8a6c25821
sherstnew <sherstnev.denis.v@gmail.com>
2023-05-12 18:25:39 +0300

    optimization

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,7 +5,7 @@ import logo from '../../static/images/academylogo.webp';
 export const Header = () => {
   return (
     <nav className={styles.header}>
-      <Link to='/'><img src={logo} alt="логотип академии" className={styles.logo} /></Link>
+      <Link to='/'><img src={logo} alt="" className={styles.logo} /></Link>
       <Link to='/teams'>Команды</Link>
       <Link to='/awards'>Награды</Link>
       <Link to='/coaches'>Тренеры</Link>
