916b00c3eaf94c9aa9a671ded52fa6c4266824f6
sherstnew <sherstnev.denis.v@gmail.com>
2023-07-24 17:10:11 +0300

    Redesign & minor fixes

--- src/components/NewsBlock/NewsBlock.jsx
+++ src/components/NewsBlock/NewsBlock.jsx
@@ -2,13 +2,11 @@ import styles from './NewsBlock.module.scss';
 import { getNews } from './../../utils/getNews';
 import { useState } from 'react';
 import { useEffect } from 'react';
-import { Loader } from './../Loader/Loader';
 import { Error } from './../Error/Error';
 import NewsCard from '../NewsCard/NewsCard';
 import arrow from '../../static/icons/arrow.svg';
 
 const NewsBlock = () => {
-
   const [news, setNews] = useState([]);
   const [status, setStatus] = useState(null);
   const [currentIndex, setCurrentIndex] = useState(0);
@@ -16,59 +14,67 @@ const NewsBlock = () => {
   useEffect(() => {
     setStatus('loading');
     getNews()
-    .then(newsData => {
-      if (newsData.length !== 0) {
-        setNews(newsData);
-        setStatus('success');
-      } else {
+      .then((newsData) => {
+        if (newsData.length !== 0) {
+          setNews(newsData);
+          setStatus('success');
+        } else {
+          setStatus('error');
+        }
+      })
+      .catch((err) => {
         setStatus('error');
-      }
-    })
-    .catch(err => {
-      setStatus('error');
-    });
+      });
   }, []);
 
-  return (
-    status === 'success'
-    ?
+  return status === 'error' ? (
+    <Error />
+  ) : (
     <div className={styles.newsBlock}>
       <NewsCard newsItem={news[currentIndex] || {}} />
       <div className={styles.switcher}>
-        <div className={styles.arrow} onClick={() => currentIndex === 0 ? setCurrentIndex(news.length - 1) : setCurrentIndex(currentIndex - 1)}>
-          <img src={arrow} alt="" className={styles.arrow__left} />
+        <div
+          className={styles.arrow}
+          onClick={() =>
+            currentIndex === 0
+              ? setCurrentIndex(news.length - 1)
+              : setCurrentIndex(currentIndex - 1)
+          }
+        >
+          <img src={arrow} alt='' className={styles.arrow__left} />
         </div>
-        <div className={styles.arrow} onClick={() => currentIndex === news.length - 1 ? setCurrentIndex(0) : setCurrentIndex(currentIndex + 1)}>
-          <img src={arrow} alt="" className={styles.arrow__right}/>
+        <div
+          className={styles.arrow}
+          onClick={() =>
+            currentIndex === news.length - 1
+              ? setCurrentIndex(0)
+              : setCurrentIndex(currentIndex + 1)
+          }
+        >
+          <img src={arrow} alt='' className={styles.arrow__right} />
         </div>
       </div>
     </div>
-    :
-    status === 'error'
-    ?
-    <Error />
-    :
-    <Loader />
-    // status === 'success'
-    // ?
-    // <div className={styles.newsBlock}>
-    //   <NewsCard newsItem={news[currentIndex]} />
-    //   <div className={styles.switcher}>
-    //     <div className={styles.arrow} onClick={() => currentIndex === 0 ? setCurrentIndex(news.length - 1) : setCurrentIndex(currentIndex - 1)}>
-    //       <img src={arrow} alt="" className={styles.arrow__left} />
-    //     </div>
-    //     <div className={styles.arrow} onClick={() => currentIndex === news.length - 1 ? setCurrentIndex(0) : setCurrentIndex(currentIndex + 1)}>
-    //       <img src={arrow} alt="" className={styles.arrow__right}/>
-    //     </div>
-    //   </div>
-    // </div>
-    // :
-    // status === 'error'
-    // ?
-    // <Error />
-    // :
-    // <Loader />
-   );
+  );
+  // status === 'success'
+  // ?
+  // <div className={styles.newsBlock}>
+  //   <NewsCard newsItem={news[currentIndex]} />
+  //   <div className={styles.switcher}>
+  //     <div className={styles.arrow} onClick={() => currentIndex === 0 ? setCurrentIndex(news.length - 1) : setCurrentIndex(currentIndex - 1)}>
+  //       <img src={arrow} alt="" className={styles.arrow__left} />
+  //     </div>
+  //     <div className={styles.arrow} onClick={() => currentIndex === news.length - 1 ? setCurrentIndex(0) : setCurrentIndex(currentIndex + 1)}>
+  //       <img src={arrow} alt="" className={styles.arrow__right}/>
+  //     </div>
+  //   </div>
+  // </div>
+  // :
+  // status === 'error'
+  // ?
+  // <Error />
+  // :
+  // <Loader />
 };
 
 export default NewsBlock;
